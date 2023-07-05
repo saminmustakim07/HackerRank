a2f2e5ea32b8de11483957ccfc964e269453c73f
Md. Samin Mustakim <137746125+saminmustakim07@users.noreply.github.com>
2023-07-05 23:48:33 +0600

    Add files via upload

---
+++ Basic_Data_Types.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define fast_io ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+int main () {
+
+    fast_io;
+
+    int value_1;
+    long value_2;
+    char value_3;
+    float value_4;
+    double value_5;
+
+    scanf ("%d %ld %c %f %lf", &value_1, &value_2, &value_3, &value_4, &value_5);
+
+    printf ("%d\n%ld\n%c\n%f\n%lf\n", value_1, value_2, value_3, value_4, value_5);
+
+    return 0;
+
+}
---
+++ Input_and_Output.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define fast_io ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+int main () {
+
+    fast_io;
+
+    int a, b, c;
+    cin >> a >> b >> c;
+
+    cout << a + b + c << endl;
+
+    return 0;
+
+}
